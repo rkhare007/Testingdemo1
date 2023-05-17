1e10b66295950be9d86ba7a6b36ae097b47f8f11
rkhare007 <rkhare580@gmail.com>
2023-05-17 17:12:00 +0530

    Updated

--- src/test/java/org/example/junithelper/ParameterizedTest.java
+++ src/test/java/org/example/junithelper/ParameterizedTest.java
@@ -4,21 +4,36 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
+import static org.junit.Assert.*;
 
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 
 @RunWith(Parameterized.class)
 public class ParameterizedTest {
 
     StringHelper stringHelper=new StringHelper();
 
-    @Parameters
-    public static Collection<String[]> testConditions(){
+    String actual;
+    String expected;
 
+    public ParameterizedTest(String actual, String expected) {
+        this.actual = actual;
+        this.expected = expected;
+    }
+
+    @Parameters
+    public static List<String[]> testConditions(){
+        String expectedoutputs[][]={
+                {"AACD","CD"},{"ACD","CD"}
+        };
+        return Arrays.asList(expectedoutputs);
     }
 
 
     @Test
     public void testtruncateAInFirst2Positions() {
+        assertEquals(expected, stringHelper.truncateAInFirst2Positions(actual));
     }
 }
